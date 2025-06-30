3a27520dc5831c29aa56f01e3ae46ed5b6f5fdd7
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-06-30 10:19:31 +0530

    changes done

--- src/components/medical-education/EducationKnowMore.jsx
+++ src/components/medical-education/EducationKnowMore.jsx
@@ -33,14 +33,14 @@ function EducationKnowMore() {
           viewport={{ once: true }}
         >
           <p>
-            eConceptual – India’s Leading MedTech Platform for Residency
-            Students Founded with a vision by Dr. Apurv Mehra, eConceptual is a
-            pioneering initiative created to empower India’s postgraduate
-            medical residents with world-class clinical and surgical training.
-            With expert mentorship from India’s most respected faculty, the
-            platform aims to shape globally competent doctors—professionals who
-            not only lead in India but stand tall on the global stage through
-            exceptional skill and academic excellence.
+            <strong>eConceptual</strong> – India’s Leading MedTech Platform for
+            Residency Students Founded with a vision by Dr. Apurv Mehra,
+            eConceptual is a pioneering initiative created to empower India’s
+            postgraduate medical residents with world-class clinical and
+            surgical training. With expert mentorship from India’s most
+            respected faculty, the platform aims to shape globally competent
+            doctors—professionals who not only lead in India but stand tall on
+            the global stage through exceptional skill and academic excellence.
           </p>
           <button class="educationKM-glow-on-hover" type="button">
             Know More..
@@ -57,13 +57,13 @@ function EducationKnowMore() {
           viewport={{ once: true }}
         >
           <p>
-            Cerebellum Academy – Shaping the Clinicians of Tomorrow Founded by
-            pioneering faculty, Cerebellum Academy is a dedicated platform for
-            NEETPG, INICET, and FMGE aspirants. In an era of rising competition
-            and self-doubt, we focus on building strong medical concepts,
-            strategic problem-solving skills, and clinical confidence—equipping
-            future doctors not just to clear exams, but to lead as
-            compassionate, competent clinicians.
+            <strong>Cerebellum Academy</strong> – Shaping the Clinicians of
+            Tomorrow Founded by pioneering faculty, Cerebellum Academy is a
+            dedicated platform for NEETPG, INICET, and FMGE aspirants. In an era
+            of rising competition and self-doubt, we focus on building strong
+            medical concepts, strategic problem-solving skills, and clinical
+            confidence—equipping future doctors not just to clear exams, but to
+            lead as compassionate, competent clinicians.
           </p>
           <button class="educationKM-glow-on-hover" type="button">
             Know More..
